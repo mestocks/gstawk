0e3180519109490a655711c1fcd4852face5a070
Michael Stocks <mspopgen@gmail.com>
2014-07-01 15:56:51 +0100

    fixing paths

--- gstawk.sh
+++ gstawk.sh
@@ -3,6 +3,7 @@
 FILES="${@:1}"
 wrkdir=$(pwd)
 makedir=$(dirname $(readlink -f "$0"))
-echo $FILES > test.txt
-make -n -C $makedir -s stats FILENAMES=test.txt PWD=$wrkdir
-make -n -C $makedir -s print FILENAMES=test.txt PWD=$wrkdir
+TEST=$pwd"/"test.txt
+echo $FILES > $(TEST)
+make -n -C $makedir -s stats FILENAMES=$(TEST) PWD=$wrkdir
+make -n -C $makedir -s print FILENAMES=$(TEST) PWD=$wrkdir
