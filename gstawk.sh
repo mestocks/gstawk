7f96b2c2e3e4458b1fe80c889990cac5282c6876
Michael Stocks <mspopgen@gmail.com>
2014-07-01 14:12:24 +0100

    realpath

--- gstawk.sh
+++ gstawk.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 FILES="${@:1}"
+wrkdir=$(pwd)
 echo $FILES > test.txt
-make -s stats FILENAMES=test.txt
-make -s print FILENAMES=test.txt
+make -s stats FILENAMES=test.txt PWD=$wrkdir
+make -s print FILENAMES=test.txt PWD=$wrkdir
