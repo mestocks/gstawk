87ccf2c97ed34744b70bc69477c8acb78993d534
Michael Stocks <mspopgen@gmail.com>
2014-07-10 11:51:23 +0100

    min

--- gstawk.sh
+++ gstawk.sh
@@ -5,5 +5,5 @@ wrkdir=$(pwd)
 makedir=$(dirname $(readlink -f "$0"))
 TEST=$wrkdir"/"test.txt
 echo $FILES > $TEST
-make -C $makedir -s stats FILENAMES=$TEST PWD=$wrkdir
-make -C $makedir -s print FILENAMES=$TEST PWD=$wrkdir
+make -n -C $makedir -s stats FILENAMES=$TEST PWD=$wrkdir
+make -n -C $makedir -s print FILENAMES=$TEST PWD=$wrkdir
