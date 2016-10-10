f0387588812b8362e00a58d76e2e84097108981e
Michael Stocks <mspopgen@gmail.com>
2016-10-10 16:01:49 +0100

    file name

--- Makefile
+++ Makefile
@@ -8,12 +8,14 @@ EXE = chrlen gstawk nx fagrep cinv cluster make_gstawk filterFasta
 BINS = $(addprefix $(BIN),$(EXE))
 PFXS = $(addprefix $(PFX),$(EXE))
 
+Wgcc = -Wall -Wextra -Wpedantic
+
 .PHONY:	all
 all:	$(BINS) $(BIN)gstawk_make
 
 $(BIN)%:	$(SRC)%.c
 	mkdir -p $(BIN)
-	gcc -o $@ $^
+	gcc $(Wgcc) -o $@ $^
 
 $(BIN)%:	$(SRC)%.sh
 	mkdir -p $(BIN)
--- src/gstawk.c
+++ src/gstawk.c
@@ -2,65 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BSIZE 1028
+#define NCHR 1000000
+
 int cmpfunc (const void * a, const void * b) { return ( *(int*)b - *(int*)a ); }
 
-int main(int argc, char *argv[]) {
-
-  const int size = 128;
+int main(int argc, char *argv[])
+{  
   const char *dna = "ATGCatgc";
-  FILE *fp = fopen(argv[1], "r");
 
-  char buf[size];
-  int clen[1000000];
+  int f;
+  FILE *fp;
+  int chrlen[NCHR];
+  char buffer[BSIZE];
   
-  int i = 0;
-  int n = 0;
-  int l = 0;
-  int L = 0;
-  int min = 0;
-  int non = 0;
-  
-  while (fgets(buf, size, fp) != NULL) {
-    // Check for ">"
-    if (strstr(buf, ">") != NULL) {
-      // > header
-      if (i > 0) {
-	if (l >= min) {
-	  L = L + l;
-	  n++;
-	  clen[n - 1] = l; } }
-      else i = 1;
-      l = 0; }
+  for (f = 1; f < argc; f++) {
+    fp = fopen(argv[f], "r");
     
-    // Sequence
-    else {
-      int seqlen = strlen(buf);
-      if (buf[seqlen - 1] == '\n') { seqlen = seqlen - 1; }
-      l = l + seqlen;
+    
+    int i = 0;
+    int n = 0;
+    int l = 0;
+    int L = 0;
+    int min = 0;
+    int non = 0;
+    int b;
+    int seqlen;
+    
+    while (fgets(buffer, BSIZE, fp) != NULL) {
       
-      int b = 0;
-      while(buf[b] != '\0' && buf[b] != '\n') {
-	if (strchr(dna, buf[b++]) == NULL) non++; } } }
-  
-  if (l >= min) {
-    L = L + l;
-    n++;
-    clen[n - 1] = l; }
-  
-  qsort(clen, n, sizeof(int), cmpfunc);
-  
-  int c = 0;
-  int n50 = 0;
-  int n50len = 0;
-  int h = L / 2;
-  
-  while (n50 <= h) {
-    n50 = n50 + clen[c];
-    n50len = clen[c];
-    c++; }
-  
-  printf("%d %d %d %d %d %d %d\n", n, L, L / n, clen[0], c, n50len, non);
-  
- fclose(fp);
+      if (strstr(buffer, ">") != NULL) {
+	if (i > 0) {
+	  if (l >= min) {
+	    L = L + l;
+	    n++;
+	    chrlen[n - 1] = l; } }
+	else i = 1;
+	l = 0; }
+      
+      else {
+	seqlen = strlen(buffer);
+	if (buffer[seqlen - 1] == '\n') { seqlen = seqlen - 1; }
+	l = l + seqlen;
+	
+	b = 0;
+	while(buffer[b] != '\0' && buffer[b] != '\n') {
+	  if (strchr(dna, buffer[b++]) == NULL) non++; } }
+    }
+    
+    if (l >= min) {
+      L = L + l;
+      n++;
+      chrlen[n - 1] = l; }
+    
+    qsort(chrlen, n, sizeof(int), cmpfunc);
+    
+    int c = 0;
+    int n50 = 0;
+    int n50len = 0;
+    int h = L / 2;
+    
+    while (n50 <= h) {
+      n50 = n50 + chrlen[c];
+      n50len = chrlen[c];
+      c++; }
+    
+    printf("%s %d %d %d %d %d %d %d\n", argv[f], n, L, L / n, chrlen[0], c, n50len, non);
+    
+    fclose(fp);
+  }
  
- return 0; }
+ return 0;
+}
